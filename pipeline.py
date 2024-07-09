9df6ac517edcd934729646b17051140d166b24c5
sumsum1517 <samsung1517sumsum@gmail.com>
2024-07-10 02:03:32 +0500

    Updated v2

--- pipeline.py
+++ pipeline.py
@@ -18,7 +18,8 @@ llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
 
 def do_ocr(img_blob):
     model = genai.GenerativeModel("gemini-pro-vision")
-    response = model.generate_content(["Extract the text as exactly as from the image", img_blob])
+    response = model.generate_content(["You are an expert OCR software. You need to extract the text exactly as in the image. Just output the extracted text, and nothing else. If you can't find any text, then output NO TEXT DETECTED. Remeber to only output the extracted text!",
+                                        img_blob])
     return response.text
 
 def generate_response(question, answer):
