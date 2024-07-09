663f803f502a63d5a5b10c5867f4c83af59fd59e
sumsum1517 <samsung1517sumsum@gmail.com>
2024-07-10 02:02:28 +0500

    Updated

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
-template =  """<s>[INST] You are an expert teacher that provides response in JSON format. You are given a question and its answer. You need to do the following things:
+template =  """<s>[INST] You are a top expert academic professor and critic at Harvard University that rates answers against a question. You are highly critical and make sure that you provide accurate and justified rating. You ALWAYS provide response in JSON format. You are given a question and its answer. You need to do the following things:
 
     1. You need to rate the relevance of the answer to the question on a scale of 0 to {rating}. If the answers satisfies all things asked in the question, you give a good rating.
     If the answer doesnt satisfy the question, or fails to satisfy some part of it, you give a bad rating If the answer is out of context, give a 0 rating. Be highly critical of the answers. 
 
-    2. You also check for any grammatical errors or speeling mistakes as well and
+    2. You also check for any grammatical errors or spelling mistakes as well and
     deduct marks if there are alot of errors present. 
 
     3. Also, you should make sure that the answers contain detailed explanation if the question requires a detailed answer. If it is mentioned in the question that the answer should be long, then if the answer isn't long, deduct alot of marks for that.
 
-    4. Give 0 rating if the answers is out of context. I repeat, give 0 rating to the answer if it does not satisfy the question.
+    4. Give 0 rating if the answer is out of context. I repeat, give 0 rating to the answer if it does not satisfy the question. Deduct marks if the answer doesn't satisfy all parts of the question. Be highly critical and thorough when rating the answer.
 
 Below is an example JSON output of a good rating:
 
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ llm = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
 # Utility functions
 def do_ocr(img_blob):
     model = genai.GenerativeModel("gemini-pro-vision")
-    response = model.generate_content(["Extract the text as exactly as from the image. Just output the extracted text, and nothing else",
+    response = model.generate_content(["You are an expert OCR software. You need to extract the text exactly as in the image. Just output the extracted text, and nothing else. If you can't find any text, then output NO TEXT DETECTED. Remeber to only output the extracted text!",
                                         img_blob])
     return response.text
 
